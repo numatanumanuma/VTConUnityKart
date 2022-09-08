5aba8735e378b122f7f3c1722811b4923616f400
numatanumanuma <chiat18re@icloud.com>
2022-09-09 00:13:22 +0900

    Create TC Scene

---
+++ Assets/Robot/Scripts/TransformExtensions.cs
@@ -0,0 +1,26 @@
+using RosMessageTypes.Geometry;
+using RosMessageTypes.Std;
+using Unity.Robotics.Core;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+
+    public static TransformMsg ToROSTransform(this Transform tfUnity)
+    {
+        return new TransformMsg(
+            // Using vector/quaternion To<>() because Transform.To<>() doesn't use localPosition/localRotation
+            tfUnity.localPosition.To<FLU>(),
+            tfUnity.localRotation.To<FLU>());
+    }
+
+    public static TransformStampedMsg ToROSTransformStamped(this Transform tfUnity, double timeStamp)
+    {
+        return new TransformStampedMsg(
+            // new HeaderMsg(new TimeStamp(timeStamp), tfUnity.parent.gameObject.name), // ROS2
+            new HeaderMsg(ROSTransformTreePublisher.seq, new TimeStamp(timeStamp), tfUnity.parent.gameObject.name), // ROS1
+            tfUnity.gameObject.name,
+            tfUnity.ToROSTransform());
+    }
+}
